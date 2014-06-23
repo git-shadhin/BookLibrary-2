d665857ba850553cf3ecc2f558348cdd653be8a1
smedel <dioseb.m@gmail.com>
2014-06-23 12:26:56 +0200

    Ajout BDD et fichiers de génération proxy

---
+++ Livre.bat
@@ -0,0 +1,13 @@
+C:
+cd C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC
+call vcvarsall.bat
+
+cd D:\BibliothequeExamen\Bibliotheque
+svcutil D:\BibliothequeExamen\Bibliotheque\COUCHEIFAC\bin\Debug\COUCHEIFAC.dll /d:D:\BibliothequeExamen\Bibliotheque
+svcutil D:\BibliothequeExamen\Bibliotheque\COUCHEIFAC.ClsIFACLivre.wsdl D:\BibliothequeExamen\Bibliotheque\*xsd /out:D:\BibliothequeExamen\Bibliotheque\Bibliotheque\Proxies\ClsIFACLivre.cs /async /ct:System.Collections.Generic.List`1 /r:D:\BibliothequeExamen\Bibliotheque\COUCHEIFAC\bin\Debug\COUCHEBO.dll /n:*,Bibliotheque.Proxies /noConfig
+
+Pause
+del D:\BibliothequeExamen\Bibliotheque\*.wsdl
+del D:\BibliothequeExamen\Bibliotheque\*.xsd
+cd..
+Pause
